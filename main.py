fe026cacef26fe1daf88557bd8a692500e578585
Bastien Dullaerts <bastien.dullaerts@proton.me>
2025-05-25 13:59:42 +0200

    B:Movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * 
+from player import *
 
 def main():
 	pygame.init()
@@ -8,13 +9,25 @@ def main():
 	print (f"Screen width: {SCREEN_WIDTH}")
 	print (f"Screen height: {SCREEN_HEIGHT}")
 
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
+
+	clock = pygame.time.Clock()
+	dt = clock.tick(60)
+
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				return
 		screen.fill((0, 0, 0))
+		player.update(dt)
+		player.draw(screen)
+		clock.tick(60)
 		pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
