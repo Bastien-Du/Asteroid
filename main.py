aa1180fc83785f991c74e6c989bc9356d0fde583
Bastien Dullaerts <bastien.dullaerts@proton.me>
2025-05-27 22:27:20 +0200

    fix(code + game loop):refactor

--- main.py
+++ main.py
@@ -7,15 +7,29 @@ from asteroid import *
 from bullet import *
 from circleshape import *
 
-def main():
+
+def show_menu(screen):
+	running = True
+	font = pygame.font.SysFont(None, 72)
+	screen.fill("black")
+	text = font.render("Press SPACE to Start", True, "white")
+	screen.blit(text, (400, 300))   # Adjust as you like
+	pygame.display.flip()
+
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				return False     # Player closed window
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+				running = False # Player pressed SPACE to start
+	return True
+
+def game_runnning():
 	pygame.init()
 	pygame.font.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
-	print ("Starting Asteroids!")
-	print (f"Screen width: {SCREEN_WIDTH}")
-	print (f"Screen height: {SCREEN_HEIGHT}")
-
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
@@ -60,13 +74,20 @@ def main():
 						player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 						player.immunity_time = PLAYER_IMMUNITY_TIME
 					if lives == 0:
-						print("Game Over")
-						sys.exit()
+						lives = 3
+						score = 0
+						asteroids.empty()
+						drawable.empty()
+						updatable.empty()
+						return
+
+
 
 		player.immunity_time -= dt
 
 		for x in drawable:
 			x.draw(screen)
+
 		pygame.draw.rect(screen, "black", pygame.Rect(0, 0, SCREEN_WIDTH, 50))
 		lives_surface = lives_police.render('Live(s): ' + str(lives-1), False, "red")
 		screen.blit(lives_surface,(SCREEN_WIDTH - 150,10))
@@ -74,12 +95,20 @@ def main():
 		screen.blit(score_surface, (10,10))
 
 
-
 		pygame.display.flip()
 
 
 
 		dt = clock.tick(60) / 1000
+
+def main():
+	pygame.init()
+	pygame.font.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	menu = show_menu(screen)
+	if  menu == True:
+		game_runnning()
+
 
 if __name__ == "__main__":
     main()
